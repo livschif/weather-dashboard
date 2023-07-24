e62558511b2bd2a354e88e1fb066c407d1e74022
livschif <oliviaschif@gmail.com>
2023-07-24 16:32:59 -0500

    added global variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
 var APIKey = 'e699308254834bb4bef5a50692f22f49';
+var searchBarEl = $('#search-bar')
+var searchBtn = $('#search-btn')
+var pastLocationEL = $(('#past-locations'))
 
-var searchBtn = $('#')
+var currentCity;
